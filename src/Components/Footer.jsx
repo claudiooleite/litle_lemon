1d86c7de3ea51479eb89157d34efb7b2bfe4eb0d
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-05-07 12:48:52 +0200

    footer added, meta tag description added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,32 @@
-function Footer() { }
+import Image from '../Images/Logo.svg'
+
+function Footer() {
+    return (
+        <>
+            <img src={Image} alt="restaurant logo" />
+            <ul>
+                <li>Doormat navigation</li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+            <ul>
+                <li>Contact</li>
+                <li>Adress</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            <ul>
+                <li>Social Media Links</li>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+            </ul>
+        </>
+    )
+}
 
 export default Footer;
