cf0c5671cf9a1d7bf08c696f2c71c436c6884e2e
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-05-08 11:21:27 +0200

    media queries for bigger screens added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,16 +4,21 @@ import Image from "../Images/Logo.svg";
 
 function Nav() {
     return (
-        <div className='nav-main'>
-            <img id='logo-top' src={Image} alt='Logo Little Lemon restaurant' />
-            <ul className='nav-links'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
+        <div class="nav-main">
+            <img id="logo-top" src={Image} alt="Logo Little Lemon restaurant" />
+            <div class="nav-content">
+                <ul class="nav-links">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+                <div class="nav-icon">
+                    {/* <!-- Icon goes here --> */}
+                </div>
+            </div>
         </div>
     )
 }
