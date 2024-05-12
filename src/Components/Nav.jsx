4cee9bbd47906960013ed59bd4ccbfdee955d097
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-05-12 07:49:56 +0200

    sode styled

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import '../stylesheets/App.css'
 import Image from "../Images/Logo.svg";
-import { HamburgerIcon, Select } from '@chakra-ui/icons';
+import { HamburgerIcon } from '@chakra-ui/icons';
+
 
 function Nav() {
     const [showNav, setShowNav] = useState(false);
@@ -20,6 +21,7 @@ function Nav() {
                 <li><a href="#order-online">Order Online</a></li>
                 <li><a href="#login">Login</a></li>
             </ul>
+
             <div className='nav-icon' onClick={toggleNav}>
                 <HamburgerIcon />
             </div>
