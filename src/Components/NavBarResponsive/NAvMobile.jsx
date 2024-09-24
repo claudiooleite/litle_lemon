628120e9e7b7274d7d33345ced1aa1018e5023b7
Claudio Leite <claudio.leite@hotmail.com>
2024-09-24 12:18:53 +0200

    navbar restyled

--- src/Components/NavBarResponsive/NAvMobile.jsx
+++ src/Components/NavBarResponsive/NAvMobile.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import image from "../../Images/logo2.png"
 import { HamburgerIcon } from '@chakra-ui/icons';
-import { Box, Image, Spacer, Flex, Button, IconButton, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerBody, DrawerCloseButton, VStack, Link } from '@chakra-ui/react';
+import { Image, Spacer, Flex, Button, IconButton, useDisclosure, Drawer, DrawerOverlay, DrawerContent, DrawerBody, DrawerCloseButton, VStack, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
 function NavMobile() {
@@ -11,48 +11,45 @@ function NavMobile() {
     return (
         <nav>
             <Flex
-                
+                m={4}
                 alignItems='center'>
-                <Box p='4' >
-                    <RouterLink to='/'>
-                        <Image width={'3rem'} id="logo-top" src={image} alt="Logo Little Lemon restaurant" />
-                    </RouterLink>
-                </Box>
+                <RouterLink to='/'>
+                    <Image width={'2rem'} id="logo-top" src={image} alt="Logo Little Lemon restaurant" />
+                </RouterLink>
                 <Spacer />
-                <Box p='4'>
-                    <IconButton
-                        boxSize='3rem'
-                        icon={<HamburgerIcon />}
-                        variant='outline'
-                        ref={btnRef}
-                        onClick={onOpen}
-                        aria-label="Open menu"
-                    />
-                </Box>
+                <IconButton
+                    boxSize='2.4rem'
+                    icon={<HamburgerIcon />}
+                    variant='outline'
+                    ref={btnRef}
+                    onClick={onOpen}
+                    aria-label="Open menu"
+                />
+
             </Flex>
             <Drawer onClose={onClose} isOpen={isOpen} size='sm' placement='top'>
                 <DrawerOverlay />
                 <DrawerContent>
                     <DrawerCloseButton />
                     <DrawerBody>
-                        <VStack as="nav" spacing={4}>
+                        <VStack as="nav" mt={4} spacing={2}>
                             <Button variant='ghost' >
-                                <Link as={RouterLink} to='/' onClick={onClose} className='nav-item'>
+                                <Link as={RouterLink} to='/' onClick={onClose} fontSize={'md'} className='nav-item'>
                                     Home
                                 </Link>
                             </Button>
                             <Button variant='ghost' >
-                                <Link as={RouterLink} to='/bookingpage' onClick={onClose} className='nav-item'>
+                                <Link as={RouterLink} to='/bookingpage' onClick={onClose} fontSize={'md'} className='nav-item'>
                                     Reserve a Table
                                 </Link>
                             </Button>
                             <Button variant='ghost'>
-                                <Link as={RouterLink} to='/menu' onClick={onClose} className='nav-item'>
+                                <Link as={RouterLink} to='/menu' onClick={onClose} fontSize={'md'} className='nav-item'>
                                     Our Menu
                                 </Link>
                             </Button>
                             <Button variant='ghost'>
-                                <Link as={RouterLink} to='/about' onClick={onClose} className='nav-item'>
+                                <Link as={RouterLink} to='/about' onClick={onClose} fontSize={'md'} className='nav-item'>
                                     About
                                 </Link>
                             </Button>
